ed520849b26e483fdaf1f0e4b8505fb9b6226e0a
Jan Runo <jan.runo@whu.edu>
2017-04-27 12:08:25 +0200

    Adding more README and converting json keys to symbols

--- test/spec.rb
+++ test/spec.rb
@@ -1,27 +1,30 @@
 require 'minitest/autorun'
 require './lib/asianodds/login'
+require 'byebug'
 
 describe "Login" do
   before do
     @user = Asianodds::Login.new("webapiuser13", "G,'{3M+U[$uwcf4+")
+    p @user
     @invalid_user = Asianodds::Login.new("XXXXX", "XXXXX")
+    p @invalid_user
   end
 
    it "A valid User returns the valid login code 0" do
      @user.code.must_equal 0
    end
 
-   # it "An invalid User returns the invalid login code -1" do
-   #   @invalid_user.code.must_equal -1
-   # end
+    #it "An invalid User returns the invalid login code -1" do
+    #  @invalid_user.code.must_equal -1
+    #end
 
-   # it "a logged in and registered user must return true for logged in" do
-   #   @user.loggedin?.must_equal true
-   # end
+    #it "a logged in and registered user must return true for logged in" do
+    #  @user.loggedin?.must_equal true
+    #end
 
-   # it "an invalid user cannot be registered and must return false for logged in" do
-   #   @invalid_user.loggedin?.must_equal false
-   # end
+    #it "an invalid user cannot be registered and must return false for logged in" do
+    #  @invalid_user.loggedin?.must_equal false
+    #end
 
   # it "Calling GetFeeds without being logged in should raise an error" do
   #   @invalid_user.get_feeds.must_raise RuntimeError
@@ -31,8 +34,13 @@ describe "Login" do
   #   x = @user.get_feeds({})
   # end
 
-  it "get_bet_by_reference should return a bet result for a valid reference" do
-    x = @user.get_bet_by_reference("WA-1493188766490")
-    p x
-  end
+  #it "get_bet_by_reference should return a bet result for a valid reference" do
+  #  x = @user.get_bet_by_reference("WA-1493188766490")
+  #  p x
+  #end
+
+  #it "calling get_sports should return a list of sports" do
+  #  x = @user.get_sports
+  #  p x
+  #end
 end
